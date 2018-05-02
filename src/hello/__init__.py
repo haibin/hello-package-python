ccf2fe4ca82af84b7716a5d62b3ca23bcc6252d3
Liu Haibin <liu.haibin@gmail.com>
2018-05-02 22:19:43 +0800

    Add alternatives of import

--- src/hello/__init__.py
+++ src/hello/__init__.py
@@ -1,3 +1,3 @@
 # Comment out the following if we want to expose everything to the package level.
-from hello.age import *
-from hello.name import *
+from hello.age import *     # same as 'from .age import *'
+from hello.name import *    # same as 'from .name import *'
